07c14e89eb57c74d50da93262b8d22d87496142d
JasonChoi <ccc11203@gmail.com>
2019-01-08 11:22:15 +0900

    Prac5 - #define DECLARE_SINGLE, ... [ singleton pattern macro ]

--- Prac5/Include/Macro.h
+++ Prac5/Include/Macro.h
@@ -2,4 +2,24 @@
 
 #define SAFE_DELETE(p) if(p) { delete p; p = NULL;}
 #define SAFE_DELETE_ARRAY(p) if(p) { delete[] p; p = NULL;}
-//#define DECLARE_SINGLE()
+#define SAFE_RELEASE(p) if(p) { p->Release(); p = NULL; }
+#define DECLARE_SINGLE(Type) \
+	private:\
+		static Type* m_pInstance;\
+	public:\
+		static Type* GetInstance()\
+		{\
+			if(!m_pInstance)\
+				m_pInstance = new Type;\
+			return m_pInstance;\
+		}\
+		static void DestroyInstance()\
+		{\
+			SAFE_DELETE(m_pInstance);\
+		}\
+	private:\
+		Type();\
+	   ~Type();
+#define DEFINITION_SINGLE(Type) Type* Type::m_pInstance = NULL;
+#define GET_SINGLE(Type)		Type::GetInstance()
+#define DESTROY_SINGLE(Type)	Type::DestroyInstance()
