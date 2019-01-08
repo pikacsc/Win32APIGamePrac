455fbfeaa40dbe4e3264c78b8337c5ab7a66540d
JasonChoi <ccc11203@gmail.com>
2019-01-08 11:31:39 +0900

    Prac5 - Timer.h, Timer.cpp

---
+++ Prac5/Include/Core/Timer.cpp
@@ -0,0 +1,34 @@
+#include "Timer.h"
+
+DEFINITION_SINGLE(Timer)
+
+
+Timer::Timer()
+	:m_fTimeScale(1.f)
+{
+}
+
+Timer::~Timer()
+{
+}
+
+bool Timer::Init()
+{
+	QueryPerformanceFrequency(&m_tSecond);
+	QueryPerformanceCounter(&m_tTime);
+
+	m_fDeltaTime = 0.f;
+	m_fFPS = 0.f;
+	m_fFPSTime = 0.f;
+	m_iFrameMax = 60;
+	m_iFrame = 0;
+
+	return true;
+}
+
+void Timer::Update()
+{
+	LARGE_INTEGER tTime;
+	QueryPerformanceCounter(&tTime);
+	m_fDeltaTime = (tTime.QuadPart - m_tTime.QuadPart) / static_cast<float>(m_tSecond.QuadPart);
+}
