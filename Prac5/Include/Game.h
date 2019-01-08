09728cc5d91f694ef50ab1a3212ee6fe7cd63de2
JasonChoi <ccc11203@gmail.com>
2019-01-08 11:42:18 +0900

    Prac5 - Safe_Delete_VecList, Safe_Release_VecList

--- Prac5/Include/Game.h
+++ Prac5/Include/Game.h
@@ -9,4 +9,32 @@ using namespace std;
 
 #include "resource.h"
 #include "Macro.h"
+#include "Flag.h"
 #include "Types.h"
+
+
+template <typename T>
+void Safe_Delete_VecList(T& p)
+{
+	T::template iterator iter;
+	T::template iterator iterEnd = p.end();
+
+	for (iter = p.begin(); iter != iterEnd; ++iter)
+	{
+		SAFE_DELETE((*iter));
+	}
+	p.clear();
+}
+
+template <typename T>
+void Safe_Release_VecList(T& p)
+{
+	T::template iterator iter;
+	T::template iterator iterEnd = p.end();
+
+	for (iter = p.begin(); iter != iterEnd; ++iter)
+	{
+		SAFE_RELEASE((*iter));
+	}
+	p.clear();
+}
