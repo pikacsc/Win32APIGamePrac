e57e53a9ee3dae578d70429cf2906c384f698e7e
JasonChoi <ccc11203@gmail.com>
2019-01-10 23:37:46 +0900

    Prac5 - [ set constants ]

---
+++ Prac5/Include/constants.cpp
@@ -0,0 +1,24 @@
+namespace constants
+{
+
+	//MoveObj
+	extern const float MoveObj_DefaultAngle(0.f);
+	extern const float MoveObj_DefaultSpeed(100.f);
+
+
+	//Player
+	extern const float Player_DefaultPosX(100.f);
+	extern const float Player_DefaultPosY(100.f);
+	extern const float Player_DefaultSizeWidth(100.f);
+	extern const float Player_DefaultSizeHeight(100.f);
+	extern const float Player_DefaultSpeed(400.f);
+
+
+	//M_OrangeMushroom
+	extern const float M_OrangeMushroom_DefaultPosX(800.f);
+	extern const float M_OrangeMushroom_DefaultPosY(100.f);
+	extern const float M_OrangeMushroom_DefaultSizeWidth(100.f);
+	extern const float M_OrangeMushroom_DefaultSizeHeight(100.f);
+	extern const float M_OrangeMushroom_DefaultSpeed(300.f);
+
+}
---
+++ Prac5/Include/constants.h
@@ -0,0 +1,26 @@
+#pragma once
+
+namespace constants
+{
+
+	//MoveObj
+	extern const float MoveObj_DefaultAngle;
+	extern const float MoveObj_DefaultSpeed;
+
+
+	//Player
+	extern const float Player_DefaultPosX;
+	extern const float Player_DefaultPosY;
+	extern const float Player_DefaultSizeWidth;
+	extern const float Player_DefaultSizeHeight;
+	extern const float Player_DefaultSpeed;
+
+
+	//M_OrangeMushroom
+	extern const float M_OrangeMushroom_DefaultPosX;
+	extern const float M_OrangeMushroom_DefaultPosY;
+	extern const float M_OrangeMushroom_DefaultSizeWidth;
+	extern const float M_OrangeMushroom_DefaultSizeHeight;
+	extern const float M_OrangeMushroom_DefaultSpeed;
+
+}
